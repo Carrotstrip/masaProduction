93e4a83532bde911c94264db6641fdbf653442a5
Austin L Wolfgram <wolfaust@umich.edu>
2018-10-02 00:32:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['insta485'],
     include_package_data=True,
     install_requires=[
-        'Flask==0.12.2',
+        'Flask==0.12.3',
         'html5validator==0.2.8',
         'pycodestyle==2.3.1',
         'pydocstyle==2.0.0',
